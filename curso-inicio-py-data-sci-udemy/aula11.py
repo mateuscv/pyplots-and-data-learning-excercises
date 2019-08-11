24b1ff88a607a87bbf0ac81d092167c4c196de5e
mateuscv <mateusv@email.com>
2019-08-10 22:35:50 -0300

    aula 11 - salvar figuras

---
+++ curso-inicio-py-data-sci-udemy/aula11.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as pyplot
+
+x = [1,3,5,7,9]
+y = [2,3,7,1,0]
+markersSize = [20, 5, 100, 33, 10]
+
+pyplot.title("Scatterplot")
+pyplot.xlabel("Eixo X")
+pyplot.ylabel("Eixo Y")
+
+pyplot.plot(x, y, color="#FF0000", ls="--") # hexa colors: 2 chars red, 2 green, 2 blue; ls: linestyle
+pyplot.scatter(x, y, label="Meus pontos", color="blue", marker="o", s=markersSize) # s: size
+pyplot.legend()
+#pyplot.grid()
+#pyplot.show()
+pyplot.savefig("oi.png", dpi=300)
+#pyplot.savefig("oi.pdf")
